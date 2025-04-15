5beebf909c576b911916ee22c02b7286c086d294
bhargavi374 <nareddybhargavi47@gmail.com>
2025-04-15 11:24:55 +0530

    changed

--- Frontend/src/Components/Navbarcomponents/Navbar.jsx
+++ Frontend/src/Components/Navbarcomponents/Navbar.jsx
@@ -53,20 +53,20 @@ export default function Navbar() {
                 </div>
 
                 {/* Hamburger Icon (Mobile) */}
-                {/* <div className="md:hidden flex justify-end w-full">
+             <div className="md:hidden flex justify-end w-full">
                     <button onClick={toggleMenu} className={`hamburger ${isToggle ? 'open' : ''}`}>
                         <span className="hamburger-top"></span>
                         <span className="hamburger-middle"></span>
                         <span className="hamburger-bottom"></span>
                     </button>
-                </div> */}{/* Hamburger Icon (Mobile) */}
-                <div className="md:hidden">
+                </div> {/* Hamburger Icon (Mobile) */}
+                {/* <div className="md:hidden">
                     <button onClick={toggleMenu} className={`hamburger ${isToggle ? 'open' : ''}`}>
                         <span className="hamburger-top"></span>
                         <span className="hamburger-middle"></span>
                         <span className="hamburger-bottom"></span>
                     </button>
-                </div>
+                </div> */}
             </div>
 
             {/* Mobile Menu */}
